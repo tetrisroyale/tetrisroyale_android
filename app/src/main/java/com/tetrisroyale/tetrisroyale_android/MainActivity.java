102a72f84c76ca4b2a54c4671184df630c07d268
kleberbaum <kleberbaum@erebos.xyz>
2019-06-04 11:40:04 +0200

    welp, guess it's working now

--- app/src/main/java/com/tetrisroyale/tetrisroyale_android/MainActivity.java
+++ app/src/main/java/com/tetrisroyale/tetrisroyale_android/MainActivity.java
@@ -2,6 +2,13 @@ package com.tetrisroyale.tetrisroyale_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getID();
+    }
+
+    private void getID(){
+        TApolloClient.getTApolloClient().query(
+               IDqueryQuery.builder().build()).enqueue(new ApolloCall.Callback<IDqueryQuery.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<IDqueryQuery.Data> response) {
+                Log.e("ID-daten", response.data().toString());
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/tetrisroyale/tetrisroyale_android/TApolloClient.java
+++ app/src/main/java/com/tetrisroyale/tetrisroyale_android/TApolloClient.java
@@ -2,15 +2,21 @@ package com.tetrisroyale.tetrisroyale_android;
 
 import com.apollographql.apollo.ApolloClient;
 
+import okhttp3.logging.HttpLoggingInterceptor;
+
 import okhttp3.OkHttpClient;
 
 public class TApolloClient {
-    private static final String BASE_URL = "";
+    private static final String BASE_URL = "https://pharmaziegasse.at/api/graphql";
     private static ApolloClient TApolloClient;
 
     public static ApolloClient getTApolloClient(){
 
-        OkHttpClient okHttpClient = new OkHttpClient().Builder()
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
                 .build();
 
         TApolloClient = ApolloClient.builder()
@@ -18,6 +24,7 @@ public class TApolloClient {
                 .okHttpClient(okHttpClient)
                 .build();
 
+        return TApolloClient;
 
     }
 }
